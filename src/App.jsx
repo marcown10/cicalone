98b67301cbd9a0c5387f98edcf073121a6e16ebf
marcown10 <96724746+marcown10@users.noreply.github.com>
2025-10-08 17:41:46 +0200

    AI generated/updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Features from './components/Features'
-import Blog from './components/Blog'
-import Contact from './components/Contact'
+import Home from './pages/Home'
+import Skills from './pages/Skills'
+import Blog from './pages/Blog'
+import Contact from './pages/Contact'
 import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <>
+    <Router basename="/cicalone">
       <Navbar />
-      <Hero />
-      <Features />
-      <Blog />
-      <Contact />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   )
 }
 
